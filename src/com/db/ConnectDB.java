1473f8989eda136012c69ad75c1160063e16879e
Allen <1693065785@qq.com>
2018-03-30 08:52:33 +0800

    second commit

--- src/com/db/ConnectDB.java
+++ src/com/db/ConnectDB.java
@@ -9,7 +9,7 @@ public class ConnectDB{
 		Connection conn = null;
 		try{
 			Class.forName("com.mysql.jdbc.Driver");
-			String url = "jdbc:mysql://39.108.144.46:3306/login";
+			String url = "jdbc:mysql://localhost:3306/login";
 			conn = DriverManager.getConnection(url, "root", "passw0rd");
 		}catch(Exception e){
 			System.out.println("Connect Error");
